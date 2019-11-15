8460809007eb03625c27d8d4355fd15a910c510d
agussgarciaa <agussgarciaa9@gmail.com>
2019-11-15 00:42:52 -0300

    Making Erros

--- MavenWebApp/src/main/resources/net/javaguides/mavenwebapp/SimpleServet.java
+++ MavenWebApp/src/main/resources/net/javaguides/mavenwebapp/SimpleServet.java
@@ -2,6 +2,8 @@ package net.javaguides.mavenwebapp;
 
 import java.io.IOException;
 
+import java.io.IOException;
+
 import javax.servlet.ServletException;
 
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +13,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 
 import javax.servlet.http.HttpServletResponse;
+
+import java.lang.String;
 
 /**
 
@@ -29,6 +33,8 @@ public class SimpleServlet extends HttpServlet {
    protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
 
          throws ServletException, IOException {
+	   try {}
+	   catch() {}
 
       resp.setContentType("text/plain");
 
